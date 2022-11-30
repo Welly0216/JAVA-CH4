b39a03d37496b25aa1ca57f75b53b8fd377174d7
Welly0216 <sf7db7@gmail.com>
2022-11-30 23:20:46 +0800

    first commit

---
+++ src/P26.java
@@ -0,0 +1,17 @@
+
+public class P26 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car[] cars;
+		cars = new Car[2];
+		cars[0] = new Car();
+		cars[1] = new RacingCar();
+
+		for (int i = 0; i < cars.length; i++) {
+			Class cl=cars[i].getClass();
+			System.out.println("第"+i+"個物件的類別是:"+cl);
+		}
+	}
+
+}
